5f8c6fbb5f4a818bb5e8fef42d6287149643f94c
m.iken <mouadiken2@gmail.com>
2022-03-01 22:25:46 +0100

    enable/disable tenantFilter globaly

--- src/main/java/com/ramsrib/springbootmultitenant2/Application.java
+++ src/main/java/com/ramsrib/springbootmultitenant2/Application.java
@@ -2,6 +2,8 @@ package com.ramsrib.springbootmultitenant2;
 
 import java.util.Map;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,6 +11,8 @@ import org.springframework.boot.autoconfigure.orm.jpa.HibernatePropertiesCustomi
 import org.springframework.stereotype.Component;
 
 import com.ramsrib.springbootmultitenant2.filter.MyInterceptor;
+import com.ramsrib.springbootmultitenant2.service.UserService;
+import com.ramsrib.springbootmultitenant2.tenant.TenantContext;
 
 @SpringBootApplication
 public class Application {
@@ -16,6 +20,7 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+
 }
 
 @Component
